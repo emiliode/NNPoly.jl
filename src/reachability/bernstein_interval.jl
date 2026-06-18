b0a47eda06e84988a0ced55576622b701f7c5700
Emil Behnke <emil.behnke@student.kit.edu>
2026-06-18 11:41:01 +0200

    reenable shortcut

--- src/reachability/bernstein_interval.jl
+++ src/reachability/bernstein_interval.jl
@@ -511,7 +511,7 @@ end
 """
 Calculates concrete bounds for A*s + b for BernsteinPoly s with common generators.
 """
-function bounds(A::AbstractMatrix, b::AbstractVector, s::BernsteinInterval; use_shortcut=false)
+function bounds(A::AbstractMatrix, b::AbstractVector, s::BernsteinInterval; use_shortcut=true)
     mapped_interval = interval_map(
         min.(0, A),
         max.(0, A),

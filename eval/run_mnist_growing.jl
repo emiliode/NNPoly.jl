56bb5b173ddab2d049dfede7272135b706096ad5
Emil Behnke <emil.behnke@student.kit.edu>
2026-09-11 23:31:58 +0200

    use SmithMonomonBounds

--- eval/run_mnist_growing.jl
+++ eval/run_mnist_growing.jl
@@ -9,7 +9,7 @@ RUNNERID = 0 # set to 0..14
 
 println("precompiling ...")
 #solver = PolyCROWN(NP.DiffNNPolySym(common_generators = true))
-solver = PolyCROWNBern(bounds_method=Overapproximate, interval_repr=CombinedImp, poly_layers=1,  threshold=500000)
+solver = PolyCROWNBern(bounds_method=SmithBoundsMonomon, interval_repr=CombinedImp, poly_layers=1,  threshold=500000)
 properties, times, y_starts, ys, y_hists = verify_vnnlib(
     solver,
     "./eval/mnist_fc",
@@ -53,7 +53,7 @@ open(logfile, "w") do f
     println(f, "network,property,n_unfixed,result,time,steps,hist_file")
 end
 net_prop_reachedtimeout = fill(true,length(model_paths)*length(properties))
-net_prop_reachedtimeout[runnerID * 3  + 1: runnerID * 3 + 3 ]= false
+net_prop_reachedtimeout[RUNNERID * 3  + 1: RUNNERID * 3 + 3 ]= false
 
 for n_un in n_unfixed
 
